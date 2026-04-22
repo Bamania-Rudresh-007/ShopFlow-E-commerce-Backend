9b4e5f84aa367cd696d5a88303edceb522ce205b
Rudresh <bamaniarudresh2008@gmail.com>
2026-04-22 16:58:13 +0530

    install nodemon and make basic server to serve the endpoints

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ import express from "express";
 const app = express();
 const port = process.env.PORT;
 
-console.log(port)
+app.get("/", (req, res) => {
+    res.end(`Hey welcome to shop-flow-backend apis`);
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on  http://localhost:${port}`);
+})
