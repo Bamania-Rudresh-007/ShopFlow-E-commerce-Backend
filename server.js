be7201029af38cdef23130d5ecc9e88bee8d5305
Rudresh <bamaniarudresh2008@gmail.com>
2026-04-22 17:06:16 +0530

    call the connectDB method when servers runs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import "dotenv/config"
 import express from "express";
+import connectDB from "./src/config/db.js";
 
 const app = express();
 const port = process.env.PORT;
@@ -7,6 +8,8 @@ const port = process.env.PORT;
 app.get("/", (req, res) => {
     res.end(`Hey welcome to shop-flow-backend apis`);
 })
+
+connectDB()
 
 app.listen(port, () => {
     console.log(`Server is running on  http://localhost:${port}`);
