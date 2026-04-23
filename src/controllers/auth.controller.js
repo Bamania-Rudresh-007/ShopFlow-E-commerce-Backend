75eac8e05518dec1785661c80d388b610ae24ca1
Rudresh <bamaniarudresh2008@gmail.com>
2026-04-23 19:12:33 +0530

    create login controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -25,3 +25,56 @@ const register = async (req, res) => {
         res.status(500).json({ message: "Server error", error: error.message });
     }
 };
+
+
+const login = async (req, res) => {
+    try{
+        const {email, password} = req.body;
+
+        if(!email || !password){
+            return res.json("all feilds are required!");
+        }
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(401).json({
+                message: "Invlid email or password",
+            })
+        }
+
+        const isAuthenticate = await bcrypt.compare(password, user.password);
+
+        if(!isAuthenticate){
+            return res.status(401).json({
+                message: "Invalid token access denied",
+            })
+        }
+
+        const token = jwt.sign({id: user._id, email: user.email}, process.env.JWT_SECRET,{expiresIn: "15m"});
+
+
+        return res.cookie("accessToken", token,{
+            httpOnly: true,
+            sameSite: "Strict",
+        })
+        .status(200)
+        .json({
+            message: "Login successful",
+            data: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            }
+        })
+
+    }   
+    catch(err){
+        console.log("Login Error: ", err);
+        res.status(500).json({
+            message: "Server Error: ",
+            error: err.message,
+        })
+    }
+}
