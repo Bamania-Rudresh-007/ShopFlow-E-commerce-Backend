d95073807696f9f4502f3b9759b3dba5537c874d
Rudresh <bamaniarudresh2008@gmail.com>
2026-06-05 18:39:17 +0530

    add user registration functionality with email validation and token generation

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,71 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const register = async (req, res) => {
+    try{
+        const {name , email , password} = req.body;
+
+        if(!name || !email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+        const existingUser = await User.findOne({email: email});
+        if(existingUser){
+            return res.status(409).json({
+                success: false,
+                message: "User with this email already exists"
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const newUser = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        const accessToken = jwt.sign({id: newUser._id,email,}, process.env.ACCESS_TOKEN,{expiresIn: "15m"});
+
+        const refreshToken = jwt.sign({id: newUser._id,email,}, process.env.REFRESH_TOKEN,{expiresIn: "7d"});
+
+        User.refreshToken = refreshToken;
+        await User.save()
+
+         const cookieOptions = {
+            httpOnly: true,                                      
+            secure: process.env.NODE_ENV === "production",       
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            maxAge: 7 * 24 * 60 * 60 * 1000                      
+        };
+
+        const createdUser = newUser.toObject();
+        delete createdUser.password;
+        delete createdUser.refreshToken;
+
+
+        return res
+        .status(201)
+        .cookie("refreshToken", refreshToken, cookieOptions)
+        .json({
+            success: true,
+            message: "User registered successfully",
+            data: {
+                user: createdUser,
+                accessToken
+            }
+        })
+
+    }
+    catch(err){
+        console.error(err);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+    }
+}
